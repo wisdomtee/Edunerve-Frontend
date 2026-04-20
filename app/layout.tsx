ec52956845cbedfab550ba46a6ff211a78824a7b
unknown <wisdomtoyin@gmail.com>
2026-04-20 17:19:24 +0100

    Update root layout styling

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} min-h-screen antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} min-h-screen bg-slate-50 antialiased`}
       >
         {children}
       </body>
